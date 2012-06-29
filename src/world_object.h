0ab2c764718cd2b26f0336f5d7e25b67a559af98
rolf <rolf@rolf-Aspire-5745G.(none)>
2012-06-29 21:32:24 +0200

    Updated world object to (soon) support removing and inserting objects

---
+++ src/world_object.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include<list>
+class ObjectHandler;
+class game_object;
+class physical_game_object;
+class WorldObject{
+ ObjectHandler * object_handler;
+ 
+ public:
+  WorldObject(ObjectHandler * object_handler);
+ 
+  std::list <game_object *> get_near_gameobjects(game_object * origin, float distance);
+  std::list <physical_game_object *> get_near_physical_objects(physical_game_object * origin, float distance);
+  
+  void remove_object(game_object *);
+  void insert_object(game_object *);
+  void insert_object(physical_game_object *);
+  
+  std::list <physical_game_object *>  get_objects_nearby_to_point(float x, float y, float distance);
+  
+};
